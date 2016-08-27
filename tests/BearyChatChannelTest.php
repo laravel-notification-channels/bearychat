7bd610e5f7b39648f24c3bd95aeed00ad1142d91
Elf Sundae <elf.sundae@gmail.com>
2016-08-27 16:49:08 +0800

    Add BearyChatChannelTest

---
+++ tests/BearyChatChannelTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace NotificationChannels\BearyChat\Test;
+
+use Mockery;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Notifiable;
+use ElfSundae\BearyChat\Client;
+use ElfSundae\BearyChat\Message;
+use ElfSundae\BearyChat\MessageDefaults;
+use ElfSundae\BearyChat\Laravel\ClientManager;
+use NotificationChannels\BearyChat\BearyChatChannel;
+use NotificationChannels\BearyChat\Exceptions\CouldNotSendNotification;
+
+class BearyChatChannelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(BearyChatChannel::class, $this->getChannel());
+    }
+
+    protected function getChannel()
+    {
+        return new BearyChatChannel(Mockery::mock(ClientManager::class));
+    }
+
+    protected function getNotifiable()
+    {
+        return new TestNotifiable;
+    }
+}
+
+class TestNotifiable
+{
+    use Notifiable;
+
+    public function routeNotificationForBearyChat()
+    {
+        return 'user';
+    }
+}
+
+class TestNotification extends Notification
+{
+    public function toBearyChat($notifiable)
+    {
+        return (new Message)
+            ->text('text')
+            ->add('attachment content', 'attachment title');
+    }
+}
