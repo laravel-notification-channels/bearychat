6f5eb4d37895fd0aa25173532a11e0a237f779dd
Elf Sundae <elf.sundae@gmail.com>
2017-06-12 04:04:33 +0800

    Fixed contextual binding #6

--- src/BearyChatServiceProvider.php
+++ src/BearyChatServiceProvider.php
@@ -3,20 +3,9 @@
 namespace NotificationChannels\BearyChat;
 
 use Illuminate\Support\ServiceProvider;
-use ElfSundae\BearyChat\Laravel\ClientManager;
 
 class BearyChatServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        $this->app->when(BearyChatChannel::class)
-            ->needs(ClientManager::class)
-            ->give('bearychat');
-    }
-
     /**
      * Register the application services.
      */
